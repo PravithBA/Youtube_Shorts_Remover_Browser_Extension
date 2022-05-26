d7118260492ec409ca2624132167e691548f7515
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-26 21:22:38 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 try{
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if(changeInfo.status = "completed" && tab.url.startsWith('http')){
-            console.log("fuck")
+        console.log(changeInfo)
+        if(changeInfo.status = "completed" && tab.url.startsWith('https://m.youtube.com') || tab.url.startsWith('http://m.youtube.com')){
+            console.log(changeInfo)
+            console.log(tab.url)
             chrome.scripting.executeScript({
                 target: {tabId: tabId},
                 files: ['index.js'] 
