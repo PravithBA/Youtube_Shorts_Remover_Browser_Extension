df7b949f49c15356964ed3a6fae3c910611ab377
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-26 20:34:00 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
-try{let tabURL
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(!changeInfo.status && tab.url != tabURL){
-        tabURL = tab.url
-        chrome.scripting.executeScript({
-            target: {tabId: tabId},
-            files: ['index.js'] 
-        })
-    }
-})}catch(err){
+try{
+    let tabURL
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        if(!changeInfo.status && tab.url != tabURL){
+            tabURL = tab.url
+            chrome.scripting.executeScript({
+                target: {tabId: tabId},
+                files: ['index.js'] 
+            })
+        }
+    })
+}
+catch(err){
     console.log(err)
 }
