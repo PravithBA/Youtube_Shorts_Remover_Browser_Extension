77ec22fd6c44c44fd14d0cfbc588901b633fd49d
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-26 21:05:41 +0530

    Bug fix

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 try{
     let tabURL
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if(!changeInfo.status && tab.url != tabURL){
+        if(!changeInfo.status && tab.url != tabURL && tab.url.startsWith('http')){
             tabURL = tab.url
             chrome.scripting.executeScript({
                 target: {tabId: tabId},
                 files: ['index.js'] 
             })
         }
-    })
+    }
+        // ,{urls:["*://m.youtube.com/*"]}
+    )
 }
 catch(err){
     console.log(err)
